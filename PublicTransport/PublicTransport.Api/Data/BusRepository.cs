93b827517565be9fab03f33b6da9c9cd0606d698
m1k4R <39298537+m1k4R@users.noreply.github.com>
2019-06-03 23:42:25 +0200

    Created repository for TimeTable, Line, Station and Bus

---
+++ PublicTransport/PublicTransport.Api/Data/BusRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Api.Models;
+
+namespace PublicTransport.Api.Data
+{
+    public class BusRepository : IBusRepository
+    {
+        private readonly DataContext _context;
+
+        public BusRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Bus> GetBus(int id)
+        {
+            return await _context.Busses.FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<Bus>> GetBuses()
+        {
+            return await _context.Busses.ToListAsync();
+        }
+    }
+}
---
+++ PublicTransport/PublicTransport.Api/Data/LineRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PublicTransport.Api.Models;
+
+namespace PublicTransport.Api.Data
+{
+    public class LineRepository : ILineRepository
+    {
+        private readonly DataContext _context;
+
+        public LineRepository(DataContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<Line> GetLine(int id)
+        {
+            return await _context.Lines.Include(s => s.Stations).Include(b => b.Buses).FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task<IEnumerable<Line>> GetLines()
+        {
+            return await _context.Lines.Include(s => s.Stations).Include(b => b.Buses).ToListAsync();
+        }
+    }
+}
