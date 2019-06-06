24bc1ee5c78b6192a7b3f4aa28e0518d0c41bbc5
m1k4R <39298537+m1k4R@users.noreply.github.com>
2019-06-06 19:37:46 +0200

    Added interfaces in angular app

---
+++ PublicTransport-Spa/src/app/_models/ticket.ts
@@ -0,0 +1,11 @@
+import { PricelistItem } from './pricelistItem';
+import { User } from './user';
+
+export interface Ticket {
+    id: number;
+    dateOfIssue: Date;
+    ticketType: string;
+    isValid: boolean;
+    user: User;
+    priceInfo: PricelistItem;
+}
